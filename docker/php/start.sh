9b6cd62477ffa3112babf135dfd4c63fc517fc32
ddurov <danyadurovnn@gmail.com>
2023-06-11 01:17:26 +0300

    loop sync database command on start container

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -1,3 +1,14 @@
-sleep 10
-php app/cli.php orm:schema-tool:update --force
-php -S 0.0.0.0:$WEB_PORT -t web/
+while true; do
+    x=0
+    if [ $x -eq 10 ]; then
+      echo "Database are not configured"
+      exit 1
+    fi
+    php app/cli.php orm:schema-tool:update --force
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 3
+    ((x++))
+done
+php -S 0.0.0.0:"$WEB_PORT" -t web/
