06ab94c99a9933ec83bd1d671637708a291b9f64
ddurov <danyadurovnn@gmail.com>
2024-01-07 01:39:14 +0300

    автоочистка логов при перезапуске

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -1,6 +1,7 @@
+rm /tmp/ddLogs/*.log
 php app/cli.php orm:schema-tool:update --force
 if [ $? -ne 0 ]; then
     echo "Database are not configured"
     exit 61
 fi
-php -S 0.0.0.0:"$WEB_PORT" web/cli.php
+PHP_CLI_SERVER_WORKERS=64 php -S 0.0.0.0:"$WEB_PORT" web/cli.php
