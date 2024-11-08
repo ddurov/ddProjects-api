70a723ba882f193e23045eeb7a45e463c623de13
ddurov <danyadurovnn@gmail.com>
2024-11-08 23:01:09 +0300

    мелкие правки

--- service/cli.php
+++ service/cli.php
@@ -4,11 +4,10 @@ require_once "vendor/autoload.php";
 
 use Core\Database;
 use Core\Tools;
-use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
 
 try {
 	Database::getInstance()->executeCLI();
-} catch (\Doctrine\DBAL\Exception|MissingMappingDriverImplementation $e) {
+} catch (\Doctrine\DBAL\Exception $e) {
 	Tools::log(1, "CLI error: {$e->getMessage()}\n");
-	die(1);
+	die(0);
 }
